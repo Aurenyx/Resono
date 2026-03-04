0c832e44c71463d86e703ab34828e381b076f2d4
Aurenyx <govindgulhane5@gmail.com>
2026-03-04 20:06:56 +0530

    Initial Commit

--- app/src/main/java/com/mrgogu/resono/domain/repository/AuthRepository.kt
+++ app/src/main/java/com/mrgogu/resono/domain/repository/AuthRepository.kt
@@ -2,4 +2,19 @@ package com.mrgogu.resono.domain.repository
 import com.mrgogu.resono.domain.model.User
 
 interface AuthRepository {
+
+    suspend fun login(
+        email: String,
+        password: String
+    ): User
+
+    suspend fun signUp(
+        name: String,
+        email: String,
+        password: String
+    ): User
+
+    suspend fun getCurrentUser(): User?
+
+    suspend fun logOut()
 }
