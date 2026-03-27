eab2c00fdf239f694847584a1acaaa25c28e6d7e
Aurenyx <govindgulhane5@gmail.com>
2026-03-28 00:49:21 +0530

    Navigation setup for User Login using NavGraph.kt and LoginScreen.kt

---
+++ app/src/main/java/com/mrgogu/resono/ui/auth/viewmodel/HomeScreen.kt
@@ -0,0 +1,9 @@
+package com.mrgogu.resono.ui.auth.viewmodel
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text("🚀Welcome to Home Screen✨w")
+}
