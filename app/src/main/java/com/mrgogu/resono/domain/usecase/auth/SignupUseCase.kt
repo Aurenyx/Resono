65a654a73114adca928a9dbcd2e6eb2a9d12f485
Aurenyx <govindgulhane5@gmail.com>
2026-04-01 23:31:06 +0530

    Setting up the Sign up use case

--- app/src/main/java/com/mrgogu/resono/domain/usecase/auth/SignupUseCase.kt
+++ app/src/main/java/com/mrgogu/resono/domain/usecase/auth/SignupUseCase.kt
@@ -1,8 +1,11 @@
 package com.mrgogu.resono.domain.usecase.auth
 import com.mrgogu.resono.domain.model.User
 import com.mrgogu.resono.domain.repository.AuthRepository
+import javax.inject.Inject
 
-class SignupUseCase(private val repository: AuthRepository) {
+class SignupUseCase @Inject constructor(
+    private val repository: AuthRepository)
+{
     suspend operator fun invoke(
         name : String,
         email: String,
