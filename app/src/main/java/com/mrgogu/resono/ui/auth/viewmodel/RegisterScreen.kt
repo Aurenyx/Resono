6e3123a21fd2cd4e4dc60ce1550c9f39fc6e7551
Aurenyx <govindgulhane5@gmail.com>
2026-04-02 23:17:47 +0530

    Building RegisterScreen.kt for New Users

--- app/src/main/java/com/mrgogu/resono/ui/auth/viewmodel/RegisterScreen.kt
+++ app/src/main/java/com/mrgogu/resono/ui/auth/viewmodel/RegisterScreen.kt
@@ -1,11 +1,31 @@
 package com.mrgogu.resono.ui.auth.viewmodel
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 
 @Composable
 fun RegisterScreen(
     onRegisterSuccess: ()-> Unit,
     viewModel: AuthViewModel = hiltViewModel()
 ) {
+
+    val state by viewModel.state.collectAsState()
+    val name by remember { mutableStateOf("") }
+    val email by remember { mutableStateOf("") }
+    val password by remember { mutableStateOf("") }
+
+    LaunchedEffect(state.user) {
+        if(state.user != null){
+            onRegisterSuccess()
+        }
+    }
+
+
+
+
 }
