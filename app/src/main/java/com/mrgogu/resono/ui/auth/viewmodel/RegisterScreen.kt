10289a720dc9b0c7fc2a7ecc0ef3e02213534945
Aurenyx <govindgulhane5@gmail.com>
2026-04-03 11:11:00 +0530

    Building Ui for Register Screen

--- app/src/main/java/com/mrgogu/resono/ui/auth/viewmodel/RegisterScreen.kt
+++ app/src/main/java/com/mrgogu/resono/ui/auth/viewmodel/RegisterScreen.kt
@@ -1,8 +1,15 @@
 package com.mrgogu.resono.ui.auth.viewmodel
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -19,6 +26,7 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun RegisterScreen(
     onRegisterSuccess: ()-> Unit,
+    onNavigateToLogin: () -> Unit,
     viewModel: AuthViewModel = hiltViewModel()
 ) {
 
@@ -26,6 +34,7 @@ fun RegisterScreen(
     var name by remember { mutableStateOf("") }
     var email by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
+    var confirmPassword by remember { mutableStateOf("") }
 
     LaunchedEffect(state.user) {
         if(state.user != null){
@@ -33,28 +42,72 @@ fun RegisterScreen(
         }
     }
 
-    Column( modifier = Modifier.padding(16.dp)) {
+    Column( modifier = Modifier.padding(16.dp).fillMaxSize(),
+        verticalArrangement = Arrangement.Center) {
 
         OutlinedTextField(
             value = name,
             onValueChange = {name = it},
-            label = { Text("Name")}
+            label = { Text("Name")},
+            modifier = Modifier.fillMaxWidth()
         )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
         OutlinedTextField(
             value = email,
             onValueChange = {email = it},
-            label = { Text("Email")}
+            label = { Text("Email")},
+            modifier = Modifier.fillMaxWidth()
         )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
         OutlinedTextField(
             value = password,
             onValueChange = {password = it},
-            label = { Text("Password")}
+            label = { Text("Password")},
+            modifier = Modifier.fillMaxWidth()
         )
 
+        Spacer(modifier = Modifier.height(8.dp))
+
+        OutlinedTextField(
+            value = confirmPassword,
+            onValueChange = {confirmPassword = it},
+            label = { Text("Confirm Password")},
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        //Register Button
         Button(onClick = {
-                viewModel.signUp(name, email,password) }
+            if (password == confirmPassword){
+                viewModel.signUp(name, email,password)
+            }
+        }, modifier = Modifier.fillMaxWidth()
         ) {
             Text("Register")
         }
+
+        //Back to Login Screen
+        Button(onClick = { onNavigateToLogin()},
+                modifier = Modifier.fillMaxWidth())
+        {
+            Text("Already have an account? Login ")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        if(state.isLoading){
+            CircularProgressIndicator()
+        }
+        state.error?.let {
+            Text(text = it, color = MaterialTheme.colorScheme.error)
+        }
+
+
+
     }
 }
