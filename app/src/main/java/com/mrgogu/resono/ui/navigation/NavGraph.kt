c0058d91bbfe32bb5fe6cf8128618af5f0ae41f1
Aurenyx <govindgulhane5@gmail.com>
2026-04-03 12:27:59 +0530

    Handling navigation btw LoginScreen.kt and RegisterScreen

--- app/src/main/java/com/mrgogu/resono/ui/navigation/NavGraph.kt
+++ app/src/main/java/com/mrgogu/resono/ui/navigation/NavGraph.kt
@@ -9,34 +9,46 @@ import com.mrgogu.resono.ui.auth.viewmodel.RegisterScreen
 
 @Composable
 fun NavGraph() {
+
     val navController = rememberNavController()
 
     NavHost(
-        navController =  navController,
+        navController = navController,
         startDestination = "login"
-     ) {
+    ) {
 
-
+        //LOGIN SCREEN
         composable("login") {
             LoginScreen(
                 onLoginSuccess = {
                     navController.navigate("home") {
-                        popUpTo("login") { inclusive = true }
+                        popUpTo("login") { inclusive = true } // remove login from backstack
                     }
+                },
+                // 🔁 Go to Register
+                onNavigateToRegister = {
+                    navController.navigate("register")
                 }
             )
         }
 
+        // REGISTER SCREEN
         composable("register") {
             RegisterScreen(
                 onRegisterSuccess = {
-                    navController.navigate("home"){
-                        popUpTo("register"){ inclusive = true}
+                    navController.navigate("home") {
+                        popUpTo("register") { inclusive = true }
                     }
+                },
+
+                // 🔙 Back to Login
+                onNavigateToLogin = {
+                    navController.popBackStack()
                 }
             )
         }
 
+        //HOME SCREEN
         composable("home") {
             HomeScreen()
         }
