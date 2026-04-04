84cb3fd5a0f7edc0b65470c86ef8f01f918bfdad
Aurenyx <govindgulhane5@gmail.com>
2026-04-04 23:47:10 +0530

    Setting Up Auto Login feature

--- app/src/main/java/com/mrgogu/resono/ui/auth/viewmodel/AuthState.kt
+++ app/src/main/java/com/mrgogu/resono/ui/auth/viewmodel/AuthState.kt
@@ -3,5 +3,6 @@ package com.mrgogu.resono.ui.auth.viewmodel
 data class AuthState(
     val isLoading: Boolean = false,
     val user: String? = null,
-    val error: String? = null
+    val error: String? = null,
+    val isLoggedIn : Boolean =false
 )
