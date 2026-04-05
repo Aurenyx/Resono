e8e4d334f6742f9008fa4b98ba84a55cf7ff1196
Aurenyx <govindgulhane5@gmail.com>
2026-04-05 23:11:42 +0530

    Logout Use Case setup

--- app/src/main/java/com/mrgogu/resono/ui/auth/viewmodel/HomeScreen.kt
+++ app/src/main/java/com/mrgogu/resono/ui/auth/viewmodel/HomeScreen.kt
@@ -1,9 +1,21 @@
 package com.mrgogu.resono.ui.auth.viewmodel
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
-fun HomeScreen() {
-    Text("🚀Welcome to Home Screen✨w")
+fun HomeScreen(
+    viewModel: AuthViewModel = hiltViewModel()
+) {
+    Column {
+        Text("🚀Welcome to Home Screen✨w")
+
+        Button(onClick = {viewModel.logout()}) {
+            Text("Logout")
+        }
+    }
+
 }
