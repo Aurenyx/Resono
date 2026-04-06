e13ba21986da2fd388542a610c60db624ea50896
Aurenyx <govindgulhane5@gmail.com>
2026-04-06 23:20:02 +0530

    Updating everything so that User details can be handle from firebase services

--- app/src/main/java/com/mrgogu/resono/ui/auth/viewmodel/AuthState.kt
+++ app/src/main/java/com/mrgogu/resono/ui/auth/viewmodel/AuthState.kt
@@ -2,7 +2,7 @@ package com.mrgogu.resono.ui.auth.viewmodel
 
 data class AuthState(
     val isLoading: Boolean = false,
-    val user: String? = null,
+    val user: com.mrgogu.resono.domain.model.User? = null,
     val error: String? = null,
     val isLoggedIn : Boolean =false
 )
