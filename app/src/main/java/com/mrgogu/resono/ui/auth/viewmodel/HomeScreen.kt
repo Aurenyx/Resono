370a0a2fa953ed11ee0d7868deaa983f9f728f20
Aurenyx <govindgulhane5@gmail.com>
2026-04-06 23:26:56 +0530

    Displaying User name stored in  firebase

--- app/src/main/java/com/mrgogu/resono/ui/auth/viewmodel/HomeScreen.kt
+++ app/src/main/java/com/mrgogu/resono/ui/auth/viewmodel/HomeScreen.kt
@@ -4,14 +4,19 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.collectAsState
 import androidx.hilt.navigation.compose.hiltViewModel
 
 @Composable
 fun HomeScreen(
     viewModel: AuthViewModel = hiltViewModel()
 ) {
+    val state by viewModel.state.collectAsState()
     Column {
-        Text("🚀Welcome to Home Screen✨w")
+        state.user.let {
+            Text("✨Welcome ${it?.name}✨")
+        }
 
         Button(onClick = {viewModel.logout()}) {
             Text("Logout")
