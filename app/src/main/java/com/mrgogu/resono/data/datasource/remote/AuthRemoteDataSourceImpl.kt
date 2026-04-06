b195c387ff353671ad58dad663d121b6180a8537
Aurenyx <govindgulhane5@gmail.com>
2026-04-06 23:19:40 +0530

    Updating everything so that User details can be handle from firebase services

--- app/src/main/java/com/mrgogu/resono/data/datasource/remote/AuthRemoteDataSourceImpl.kt
+++ app/src/main/java/com/mrgogu/resono/data/datasource/remote/AuthRemoteDataSourceImpl.kt
@@ -3,13 +3,15 @@ package com.mrgogu.resono.data.datasource.remote
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import kotlinx.coroutines.tasks.await
+import com.google.firebase.firestore.FirebaseFirestore
 
 /*  FirebaseAuth instance is injected so this class does not create
     Firebase objects itself. This keeps the architecture flexible
     and allows dependency injection (e.g., Hilt later).
 */
 class AuthRemoteDataSourceImpl(
-    private val firebaseAuth: FirebaseAuth
+    private val firebaseAuth: FirebaseAuth,
+    private val firestore: FirebaseFirestore
 ) : AuthRemoteDataSource {
 
 /*    Authenticates a user using email and password through Firebase.
@@ -40,11 +42,9 @@ class AuthRemoteDataSourceImpl(
             .await()
         val user = result.user
 
-        user?.updateProfile(
-            com.google.firebase.auth.UserProfileChangeRequest.Builder()
-                .setDisplayName(name)
-                .build()
-        )?.await()
+        user?.let {
+            saveUserData(it.uid, name, email)
+        }
 
         return user
     }
@@ -61,5 +61,38 @@ class AuthRemoteDataSourceImpl(
  */
     override suspend fun logOut() {
         firebaseAuth.signOut()
+    }
+
+    override suspend fun saveUserData(
+        uid: String,
+        name: String,
+        email: String
+    ) {
+        val userMap = mapOf(
+            "uid" to uid,
+            "name" to name,
+            "email" to email
+        )
+
+        firestore.collection("users")
+            .document(uid)
+            .set(userMap)
+            .await()
+    }
+
+    override suspend fun getUserData(uid: String): Map<String, String>? {
+        val document = firestore.collection("users")
+            .document(uid)
+            .get()
+            .await()
+
+        val data = document.data  ?: return null
+
+        return mapOf(
+            "uid" to (data["uid"] as? String ?: ""),
+            "name" to (data["name"] as? String ?: ""),
+            "email" to (data["email"] as? String ?: ""),
+        )
+
     }
 }
